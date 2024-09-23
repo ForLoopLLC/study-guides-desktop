43643374a957aab4cd41aa381cfde2fdca101b52
Bruce Stockwell <bruce.stockwell@gmail.com>
2024-09-23 17:03:02 -0400

    Now it's feature complete

--- src/main/handlers/publishHandlers.ts
+++ src/main/handlers/publishHandlers.ts
@@ -1,12 +1,15 @@
 import { ipcMain } from 'electron';
 import { log } from '../main';
 import { getTags, touchTag } from '../lib/database/tags';
+import { getQuestions, touchQuestion } from '../lib/database/questions';
 import { createTagIndex } from '../lib/database/search';
 import { publishTagIndex } from '../lib/search/tags';
 import { getUsers } from '../lib/database/users';
 import { createUserIndex } from '../lib/database/search';
 import { publishUsersIndex } from '../lib/search/users';
-import { User, Tag } from '@prisma/client';
+import { publishQuestionIndex } from '../lib/search/questions';
+import { createQuestionIndex } from '../lib/database/search';
+import { User, Tag, Question } from '@prisma/client';
 
 ipcMain.handle('publish-tags-index', async (event, { filter, query }) => {
   try {
@@ -85,6 +88,55 @@ ipcMain.handle('publish-users-index', async (event, { filter, query }) => {
     const err = error as Error;
     log.error('publish', `Error publishing index: ${err.message}`);
     event.sender.send('publish-users-index-error', { message: err.message });
+    throw new Error('Failed to publish index.');
+  }
+});
+
+ipcMain.handle('publish-questions-index', async (event, { filter, query }) => {
+  try {
+    let page = 1;
+    const limit = 500;
+    let allQuestions: Question[] = [];
+    let hasMore = true;
+    let totalProcessed = 0;
+
+    while (hasMore) {
+      const { data: questions } = await getQuestions(
+        page,
+        limit,
+        filter,
+        query,
+      );
+      if (questions.length === 0) {
+        hasMore = false;
+        break;
+      }
+
+      allQuestions = allQuestions.concat(questions);
+      page += 1;
+
+      const touchedQuestions = await Promise.all(
+        questions.map((question) => touchQuestion(question.id)),
+      );
+      const indexes = await Promise.all(
+        touchedQuestions.map((question) => createQuestionIndex(question)),
+      );
+      await publishTagIndex(indexes.filter((index) => index !== null));
+
+      totalProcessed += questions.length;
+      const payload = { page, totalProcessed };
+      event.sender.send('publish-questions-index-progress', payload);
+      log.info('publish', `Processed ${totalProcessed} questions.`);
+    }
+
+    event.sender.send('publish-questions-index-complete', { totalProcessed });
+    log.info('publish', `Index published with ${totalProcessed} questions.`);
+  } catch (error) {
+    const err = error as Error;
+    log.error('publish', `Error publishing index: ${err.message}`);
+    event.sender.send('publish-questions-index-error', {
+      message: err.message,
+    });
     throw new Error('Failed to publish index.');
   }
 });
